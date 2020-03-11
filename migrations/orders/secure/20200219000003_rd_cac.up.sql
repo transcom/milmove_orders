95bfe916be0f14b883ddf58813149a34de08c45d
Chris Gilmer <chris@truss.works>
2020-03-11 13:52:00 -0700

    Update cac migrations

--- migrations/orders/secure/20200219000003_rd_cac.up.sql
+++ migrations/orders/secure/20200219000003_rd_cac.up.sql
@@ -1,6 +1,5 @@
-
--- This migration allows a CAC cert to have read/write access to all orders and the prime API.
--- The Orders API and the Prime API use client certificate authentication. Only certificates
+-- This migration allows a CAC cert to have read/write access to all orders.
+-- The Orders API uses client certificate authentication. Only certificates
 -- signed by a trusted CA (such as DISA) are allowed which includes CACs.
 -- Using a person's CAC as the certificate is a convenient way to permit a
 -- single trusted individual to interact with the Orders API and the Prime API. Eventually
@@ -9,10 +8,7 @@ INSERT INTO public.client_certs (
 	id,
 	sha256_digest,
 	subject,
-	allow_dps_auth_api,
 	allow_orders_api,
-	created_at,
-	updated_at,
 	allow_air_force_orders_read,
 	allow_air_force_orders_write,
 	allow_army_orders_read,
@@ -23,15 +19,13 @@ INSERT INTO public.client_certs (
 	allow_marine_corps_orders_write,
 	allow_navy_orders_read,
 	allow_navy_orders_write,
-	allow_prime)
+	created_at,
+	updated_at)
 VALUES (
 	'f2310c80-95b7-4e8e-b40a-733db0f367fb',
 	'f2658a432de1f84813f4372d93873a328925df5a4fdc4ee948a025cb9520a83c',
 	'CN=rdhariwal,OU=DoD+OU=PKI+OU=CONTRACTOR,O=U.S. Government,C=US',
-	false,
 	true,
-	now(),
-	now(),
 	true,
 	true,
 	true,
@@ -42,4 +36,5 @@ VALUES (
 	true,
 	true,
 	true,
-	true);
+	now(),
+	now());
